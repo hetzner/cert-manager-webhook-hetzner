caddb4babecdfa75bbbb752ac43e235a763e1b08
lukasmetzner <lukas.metzner@hetzner-cloud.de>
2025-10-07 15:01:38 +0200

    update version range

--- dev/providers.tf
+++ dev/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "< 4.0.0"
+      version = ">= 3.0.0, < 4.0.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
